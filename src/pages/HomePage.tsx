3e498ac623b9baa2f20377cecc768f22cedbd36e
Agustin Dana <agusidana04@gmail.com>
2026-04-07 16:26:34 -0300

    variables css para seguir la paleta de colores

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,11 @@ import Prueba from '../components/componente.tsx';
 export default function HomePage() {
   return (
     <>
-      <div className="bg-gray-500 min-w-full my-auto min-h-full flex justify-center align-middle">
+      <div className="bg-gray-500 w-full h-screen flex justify-center items-center">
         <Prueba></Prueba>
+        <div className="border-primary border-2 bg-gray-100 text-2xl text-secondary">
+          Agustin esta probando los colores
+        </div>
       </div>
     </>
   );
