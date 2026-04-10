dcc8d7f38ba3155f8400ea19b819d2c26a28a1c4
LuisParmigiani <parmigianiluis04@gmail.com>
2026-04-10 18:44:26 -0300

    pay

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Dashboard from './pages/customer/Dashboard.tsx';
 import Directions from './pages/customer/Directions.tsx';
 import NewOrder from './pages/customer/NewOrder.tsx';
 import Orders from './pages/customer/Orders.tsx';
+import Pay from './pages/customer/pay.tsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/customer/orders" element={<Orders />} />
           <Route path="/customer/newOrder" element={<NewOrder />} />
           <Route path="/customer/directions" element={<Directions />} />
+          <Route path='/customer/pay' element={<Pay />} />
         </Routes>
       </Router>
     </>
